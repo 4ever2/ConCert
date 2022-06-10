fc1b24a0e9ce5d95e199b4ae1e6d6740f5705647
4ever2 <3417013+4ever2@users.noreply.github.com>
2022-06-10 13:01:32 +0200

    no message

--- examples/eip20/EIP20RustExtraction.v
+++ examples/eip20/EIP20RustExtraction.v
@@ -22,7 +22,7 @@ Definition should_inline kn :=
   || if String.index 0 "setter_from_getter" (string_of_kername kn) then true else false.
 
 Definition map_find : string :=
-  <$ "fn ##name##<K : 'a + Ord, V : Clone>(&'a self, key : K , m : immutable_map::TreeMap<K, V>) -> Option<V> {";
+  <$ "fn ##name##<K : 'a + Ord, V : Clone>(&'a self, key : K , m : TreeMap<K, V>) -> Option<V> {";
      "  match m.get(&key) {";
      "    Some(v) => Some(v.clone()),";
      "    None => None";
@@ -30,12 +30,12 @@ Definition map_find : string :=
      "}" $>.
 
 Definition map_add : string :=
-  <$"fn ##name##<K : Clone + Ord, V : Clone>(&'a self, key : K, value : V, m : immutable_map::TreeMap<K, V>) -> TreeMap<K, V> {";
+  <$"fn ##name##<K : Clone + Ord, V : Clone>(&'a self, key : K, value : V, m : TreeMap<K, V>) -> TreeMap<K, V> {";
     "  m.insert(key,value)";
     "}"$>.
 
 Definition map_empty : string :=
-  <$ "fn ##name##<K, V> (&'a self, _ : ()) -> immutable_map::TreeMap<K, V> {";
+  <$ "fn ##name##<K, V> (&'a self, _ : ()) -> TreeMap<K, V> {";
      "  TreeMap::new()";
      "}" $>.
 
@@ -66,75 +66,3 @@ MetaCoq Run (concordium_extraction
                     ++ ConcordiumRemap.remap_blockchain_inductives
                     ++ remap_fmap))
                should_inline).
-
-(*
-Definition remap_nat : remapped_inductive:=
-  {| re_ind_name := "u64";
-     re_ind_ctors := ["0"; "__nat_succ"];
-     re_ind_match := Some "__nat_elim!" |}.
-
-Definition remap_positive : remapped_inductive :=
-  {| re_ind_name := "u64";
-     re_ind_ctors := ["__pos_onebit"; "__pos_zerobit"; "1"];
-     re_ind_match := Some "__pos_elim!"
-  |}.
-
-Definition remap_Z : remapped_inductive :=
-  {| re_ind_name := "i64";
-     re_ind_ctors := ["0"; "__Z_frompos"; "__Z_fromneg"];
-     re_ind_match := Some "__Z_elim!";
-  |}.
-
-Definition remap_bool : remapped_inductive :=
-  {| re_ind_name := "bool";
-     re_ind_ctors := ["true"; "false"];
-     re_ind_match := None
-  |}.
-
-Definition remap_pair : remapped_inductive :=
-  {| re_ind_name := "__pair";
-     re_ind_ctors := ["__mk_pair"];
-     re_ind_match := Some "__pair_elim!"
-  |}.
-
-Definition remap_option : remapped_inductive :=
-  {| re_ind_name := "Option";
-     re_ind_ctors := ["Some"; "None"];
-     re_ind_match := None
-  |}.
-
-Definition remap_unit : remapped_inductive :=
-  {| re_ind_name := "()";
-     re_ind_ctors := ["()"];
-     re_ind_match := None
-  |}.
-
-Definition remap_string : remapped_inductive :=
-  {| re_ind_name := "&'a String";
-     re_ind_ctors := [];
-     re_ind_match := None |}.
-
-Definition remap_std_types :=
-  [ (<! nat !>, remap_nat)
-  ; (<! positive !>, remap_positive)
-  ; (<! Z !>,  remap_Z)
-  ; (<! bool !>, remap_bool)
-  ; (<! prod !>, remap_pair)
-  ; (<! option !>, remap_option)
-  ; (<! unit !>, remap_unit)
-  ; (<! string !>, remap_string) ].
-
-Definition remap_SerializedValue : remapped_inductive :=
-  {| re_ind_name := "&'a SerializedValue<'a>";
-     re_ind_ctors := ["__SerializedValue__Is__Opaque"];
-     re_ind_match := None |}.
-
-Definition remap_ActionBody : remapped_inductive :=
-  {| re_ind_name := "ActionBody<'a>";
-     re_ind_ctors := ["ActionBody::Transfer"; "ActionBody::Call"; "__Deploy__Is__Not__Supported"];
-     re_ind_match := None |}.
-
-Definition remap_blockchain_inductives : list (inductive * remapped_inductive) :=
-  [ (<! Serializable.SerializedValue !>, remap_SerializedValue);
-    (<! @ActionBody !>, remap_ActionBody) ].
-*)
