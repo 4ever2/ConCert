0170ce859812eaebc5fa005b79601c1359678cea
4ever2 <3417013+4ever2@users.noreply.github.com>
2023-12-28 22:01:34 +0100

    Add hacspec lib

---
+++ examples/hacspec/HacspecConcordium.v
@@ -0,0 +1,33 @@
+(* [[file:concordium.org::* concordium - Coq code][concordium - Coq code:1]] *)
+
+(** This file was automatically generated using Hacspec **)
+From ConCert.Examples.Hacspec Require Import HacspecLib MachineIntegers.
+From Coq Require Import ZArith.
+From Coq Require Import List.
+Import ListNotations.
+Open Scope Z_scope.
+Open Scope bool_scope.
+Open Scope hacspec_scope.
+
+(* concordium - Coq code:1 ends here *)
+
+(* [[file:concordium.org::* concordium - Coq code][concordium - Coq code:2]] *)
+From ConCert.Examples.Hacspec Require Import ConcordiumPrims.
+Export ConcordiumPrims.
+(* concordium - Coq code:2 ends here *)
+
+(* [[file:concordium.org::* concordium - Coq code][concordium - Coq code:3]] *)
+From ConCert.Examples.Hacspec Require Import ConcordiumTypes.
+Export ConcordiumTypes.
+(* concordium - Coq code:3 ends here *)
+
+(* [[file:concordium.org::* concordium - Coq code][concordium - Coq code:4]] *)
+From ConCert.Examples.Hacspec Require Import ConcordiumTraits.
+Export ConcordiumTraits.
+(* concordium - Coq code:4 ends here *)
+
+(* [[file:concordium.org::* concordium - Coq code][concordium - Coq code:5]] *)
+From ConCert.Examples.Hacspec Require Import ConcordiumImpls.
+Export ConcordiumImpls.
+(* concordium - Coq code:5 ends here *)
+
