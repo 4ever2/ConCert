054bcf39a0371cfbbaf991f5bbfeac099b843676
4ever2 <3417013+4ever2@users.noreply.github.com>
2024-07-10 15:40:01 +0200

    Clean up nix files

--- .nix/config.nix
+++ .nix/config.nix
@@ -4,39 +4,10 @@
   ## unless you made an automated or manual update
   ## to another supported format.
 
-  ## The attribute to build from the local sources,
-  ## either using nixpkgs data or the overlays located in `.nix/coq-overlays`
-  ## Will determine the default main-job of the bundles defined below
   attribute = "ConCert";
 
-  ## If you want to select a different attribute (to build from the local sources as well)
-  ## when calling `nix-shell` and `nix-build` without the `--argstr job` argument
-  # shell-attribute = "{{nix_name}}";
-
-  ## Maybe the shortname of the library is different from
-  ## the name of the nixpkgs attribute, if so, set it here:
-  # pname = "ConCert";
-
-  ## Lists the dependencies, phrased in terms of nix attributes.
-  ## No need to list Coq, it is already included.
-  ## These dependencies will systematically be added to the currently
-  ## known dependencies, if any more than Coq.
-  ## /!\ Remove this field as soon as the package is available on nixpkgs.
-  ## /!\ Manual overlays in `.nix/coq-overlays` should be preferred then.
-  # buildInputs = [ ];
-
-  ## Indicate the relative location of your _CoqProject
-  ## If not specified, it defaults to "_CoqProject"
-  # coqproject = "_CoqProject";
-
-  ## select an entry to build in the following `bundles` set
-  ## defaults to "default"
   default-bundle = "8.17";
 
-  ## write one `bundles.name` attribute set per
-  ## alternative configuration
-  ## When generating GitHub Action CI, one workflow file
-  ## will be created per bundle
   bundles."8.17" = {
     coqPackages.coq.override.version = "8.17";
     coqPackages.metacoq.override.version = "1.3.1-8.17";
@@ -57,7 +28,6 @@
   };
 
   ## Cachix caches to use in CI
-  ## Below we list some standard ones
   cachix.coq = {};
   cachix.coq-community = {};
   cachix.metacoq = {};
